242181989ae762ad5b07e028a81fddcc9ee5e447
Francisco <franciscomuniozs@gmail.com>
2019-05-13 15:52:18 -0400

    Last update - removed turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -7,10 +7,9 @@
 //= require datatables
 //= require bootstrap-datepicker
 //= require toastr
-//= require turbolinks
 //= require_tree .
 
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
   toastr.options = {
     "closeButton": false,
     "debug": false,
@@ -27,7 +26,16 @@ $(document).on('turbolinks:load', function(){
   }
 });
 
-$(document).on('turbolinks:load', function() {
+$(document).ready(function(){
+  $(".alert").alert();
+  window.setTimeout(function() {
+    $(".alert").slideUp(function() {
+      $(".alert").alert('close');
+    });
+  }, 5000);
+});
+
+$(document).ready(function() {
   $("#dttb").dataTable({
     language: {
         url: 'https://cdn.datatables.net/plug-ins/1.10.19/i18n/Spanish.json'
@@ -36,28 +44,28 @@ $(document).on('turbolinks:load', function() {
   });
 });
 
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
   $('.datepicker0').datepicker({
     format: "dd/mm/yyyy",
     language: "es"
   });
 });
 
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
   $('.datepicker1').datepicker({
     format: "dd/mm/yyyy",
     language: "es"
   });
 });
 
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
   $('.datepicker2').datepicker({
     format: "dd/mm/yyyy",
     language: "es"
   });
 });
 
-$(document).on('turbolinks:load', function(){
+$(document).ready(function(){
   $('.notification_date').datepicker({
     format: "dd/mm/yyyy",
     language: "es"
--- app/controllers/guarantees_controller.rb
+++ app/controllers/guarantees_controller.rb
@@ -1,6 +1,6 @@
 class GuaranteesController < ApplicationController
   before_action :set_guarantee, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!
+  before_action :authenticate_user!, except: [ :index ]
 
   # GET /guarantees
   # GET /guarantees.json
